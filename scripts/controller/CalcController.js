7514229091ec47ace53f0188affeafd950924d2a
guilherme-vieira-flooxai <guilherme.vieira.flooxai@gmail.com>
2025-02-26 10:34:57 -0300

    fix: display

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,76 +1,136 @@
 class CalcController {
 
     constructor(){
-    this._lastOperator = '';
-    this._lastNumber = '';
-    
-    this._operation = []; 
-    this._locale = 'pt-BR';
-    this._displayCalcEl = document.querySelector("#display");
-    this._dateEl = document.querySelector("#data");
-    this._timeEl = document.querySelector("#hora");
-    this._currentDate; 
-    this.initialize();
-    this.initButtonsEvents();
-    this.initKeyboard();
-    }
-     
-    initialize(){
-    
-        this.setDisplayDateTime();
-    
-        setInterval(()=>{
-    
-           this.setDisplayDateTime();
-        }, 1000);
-    
-    }
-    
-initKeyboard(){
-//ver erro
-    document.addEventListener('keyup', e=>{
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
+        this._lastOperator = '';
+        this._lastNumber = '';
+        this._operation = [];
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
+        this.initButtonsEvents();
+        this.initKeyboard();
 
-        switch (e.key) {
-            case   'Escape':
-                this.clearAll();
-            break;
-            case   'Backspace':
-                this.clearEntry();
-            break;
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-            case '%':
-                this.addOperation(e.key);
-            break;
-            case 'Enter':
-            case '=':
-                this.calc();
-            break;
-    
-            case 'ponto':
-            case ',':
-                this.addDot();
-            break;
-    
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-                this.addOperation(parseInt(e.key));
-                break;
-    
+    }
+
+copyToClipboard() {
+
+        if (navigator.clipboard) {
+            navigator.clipboard.writeText(this.displayCalc);
         }
-    })    
-}
 
+    }
+
+pasteFromClipboard() {
+
+        document.addEventListener('paste', e => {
+
+            let text = e.clipboardData.getData('Text');
+
+            this.displayCalc = parseFloat(text);
+
+        });
+
+    }
+
+     
+initialize() {
+
+        this.setDisplayDateTime();
+
+        setInterval(() => {
+
+            this.setDisplayDateTime();
+
+        }, 1000);
+
+        this.setLastNumberToDisplay();
+        this.pasteFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn => {
+
+            btn.addEventListener('dblclick', e => {
+
+                this.toggleAudio();
+
+            });
+
+        });
+
+    }
+    
+toggleAudio() {
+
+        this._audioOnOff = !this._audioOnOff;
+
+    }
+
+playAudio() {
+
+        if (this._audioOnOff) {
+            
+            this._audio.currentTime = 0;
+            this._audio.play();
+
+        }
+
+    }
+
+initKeyboard() {
+
+        document.addEventListener('keyup', e => {
+
+            this.playAudio();
+
+            switch (e.key) {
+                case 'Escape':
+                    this.clearAll();
+                    break;
+                case 'Backspace':
+                    this.clearEntry();
+                    break;
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+                    this.addOperation(e.key);
+                    break;
+                case 'Enter':
+                case '=':
+                    this.calc();
+                    break;
+                case '.':
+                case ',':
+                    this.addDot();
+                    break;
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));
+                    break;
+
+                case 'c':
+                    if (e.ctrlKey) this.copyToClipboard();
+                    break;
+                
+            }
+
+        });
+
+    }
+    
     addEventListenerAll(element, events, fn){
     
         events.split(' ').forEach(event => {
@@ -129,80 +189,89 @@ initKeyboard(){
         }
     }
        
-    getResult(){
-    
-        return eval(this._operation.join(""));;
-    
+    getResult() {
+
+        try {
+            return eval(this._operation.join(""));
+        } catch (e) {
+            setTimeout(() => this.setError(), 1);
+        }
+
     }
     
-    calc(){
+    calc() {
+
         let last = '';
-    
-        this._lastOperator = this.getlastItem(); 
-    
+
+        this._lastOperator = this.getLastItem();
+
         if (this._operation.length < 3) {
-    
+
             let firstItem = this._operation[0];
             this._operation = [firstItem, this._lastOperator, this._lastNumber];
+
         }
-    
-        if (this._operation.length > 3){
-            last = this._operation.pop();  
-           this._lastNumber = this.getResult();
-        } else if (this._operation.length == 3){
-            this._lastNumber = this.getlastItem(false);
-    
+
+        if (this._operation.length > 3) {
+
+            last = this._operation.pop();
+            this._lastNumber = this.getResult();
+
+        } else if (this._operation.length === 3) {
+
+            this._lastNumber = this.getLastItem(false);
+
         }
-    
-    
+
         let result = this.getResult();
-    
-        if (last == '%'){
+
+        if (last === '%') {
+
             result /= 100;
-        this._operation = [result];
-    
-        } else {
             this._operation = [result];
-    
+
+        } else {
+
+            this._operation = [result];
+
             if (last) this._operation.push(last);
-            
+
         }
-    
-    
-    
+
         this.setLastNumberToDisplay();
-    
+
     }
     
-    getlastItem(iaOperator = true) {
-    
+    getLastItem(isOperator = true) {
+
         let lastItem;
-    
-        for (let i = this._operation.length-1; i >= 0; i--){
-    
-            if (this.isOperator(this._operation(i)) == isOperator){
-                lastItem = this._operation(i);
+
+        for (let i = this._operation.length - 1; i >= 0; i--) {
+
+            if (this.isOperator(this._operation[i]) === isOperator) {
+                lastItem = this._operation[i];
                 break;
             }
+
         }
-    
-        if (!lastItem){
-    
+
+        if (!lastItem) {
+
             lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
-    
+
         }
-    
+
         return lastItem;
-    
+
     }
-    setLastNumberToDisplay(){
-    
-        let lastNumber = this.getlastItem(false);
-    
+    setLastNumberToDisplay() {
+
+        let lastNumber = this.getLastItem(false);
+
         if (!lastNumber) lastNumber = 0;
-    
+
         this.displayCalc = lastNumber;
-    
+
     }
     
     addOperation(value){
@@ -314,30 +383,28 @@ initKeyboard(){
         }
     }
     
-    initButtonsEvents(){
-    
-       let buttons = document.querySelectorAll("#buttons > g, #parts > g");
-    
-        console.log(buttons);
-    
-    
-        buttons.forEach((btn, index)=>{
-    
-        this.addEventListenerAll(btn, 'click drag', e =>{
-    
-            console.log(btn.className.baseVal.replace("btn-", ""));
-    
-      
+    initButtonsEvents() {
+
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index) => {
+
+            this.addEventListenerAll(btn, 'click drag', e => {
+
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+
+                this.execBtn(textBtn);
+
+            });
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
+
+                btn.style.cursor = "pointer";
+
+            });
+
         });
-            
-        this.addEventListenerAll(btn, "mouseouver mouseup, mousedown", e => {
-    
-            btn.style.cursor = "pointer";
-        
-        });
-    
-        });
-    
+
     }
     
     
@@ -373,9 +440,15 @@ initKeyboard(){
         return this._displayCalcEl.innerHTML; 
     }
     
-    set displayCalc(value){
-        this._displayCalcEl.innerHTML  = valor;
-    
+    set displayCalc(value) {
+
+        if (value.toString().length > 10) {
+            this.setError();
+            return false;
+        }
+
+        this._displayCalcEl.innerHTML = value;
+        
     }
     get currentDate(){
         return new Date();
